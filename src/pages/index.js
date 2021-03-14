3f901ff4da319145999a714a98e9da9f24e3538a
Gita Alekhya Paul <54375111+gitaalekhyapaul@users.noreply.github.com>
2021-03-15 01:21:48 +0530

    feat: improve home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -58,7 +58,7 @@ const features = [
 function Feature({ imageUrl, title, description }) {
   const imgUrl = useBaseUrl(imageUrl);
   return (
-    <div className={clsx("col col--4", styles.feature)}>
+    <div className={clsx("col col--6", styles.feature)}>
       {imgUrl && (
         <div className="text--center">
           <img className={styles.featureImage} src={imgUrl} alt={title} />
