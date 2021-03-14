8d9e659b0980156f6484e8475540b08ecf188411
Gita Alekhya Paul <54375111+gitaalekhyapaul@users.noreply.github.com>
2021-03-15 01:27:26 +0530

    feat: update footer

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -56,6 +56,10 @@ module.exports = {
               label: "amplify-util-mock",
               to: "docs/amplify-util-mock/",
             },
+            {
+              label: "amplify-cli",
+              to: "docs/amplify-cli/",
+            },
           ],
         },
         {
